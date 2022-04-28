17c3d6be6a0f5a7a8e2dfdd14064b24c232d0912
Dashiell Stander <dash.stander@gmail.com>
2022-04-27 20:14:54 -0700

    First pass, sure there are a lot of bugs
    Signed-off-by: Dashiell Stander <dash.stander@gmail.com>

--- block_recurrent_transformer/transformer.py
+++ block_recurrent_transformer/transformer.py
@@ -1,12 +1,11 @@
 from collections import namedtuple
-from einops import rearrange, repeat
+from einops import rearrange
 import torch
-import torch.nn.functional as F
-from torch import einsum, nn
+from torch import nn
 from torchtyping import TensorType
 from typing import Optional
 from x_transformers.x_transformers import (
-    Attention, exists, default, FeedForward, max_neg_value, l2norm, init_zero_, RotaryEmbedding, AlibiPositionalBias
+    Attention, default, FeedForward, RotaryEmbedding, AlibiPositionalBias
 )
 
 
@@ -42,6 +41,26 @@ def apply_rotary_pos_emb(t, freqs):
 
 
 
+
+class BRAGate(nn.Module):
+    """Poor man's LSTM
+    """
+
+    def __init__(self, dim):
+        super().__init__()
+
+        self.main_proj = nn.Linear(dim, dim, bias = True)
+        self.input_proj = nn.Linear(dim, dim, bias = True)
+        self.forget_proj = nn.Linear(dim, dim, bias = True)
+    
+    def forward(self, x, state):
+        z = torch.tanh(self.main_proj(x))
+        i = torch.sigmoid(self.input_proj(x) - 1)
+        f = torch.sigmoid(self.forget_proj(x) + 1)
+        return torch.mul(state, f) + torch.mul(z, i)
+
+
+
 class BlockRecurrentAttention(nn.Module):
     def __init__(
         self,
@@ -68,8 +87,8 @@ class BlockRecurrentAttention(nn.Module):
         self.input_state_cross_attn = Attention(dim, heads = heads, causal = False, **attn_kwargs)
         self.state_input_cross_attn = Attention(dim_state, heads = heads, causal = False, **attn_kwargs)
 
-        self.proj_gate = nn.LSTMCell(dim, dim, bias = True)
-        self.ff_gate = nn.LSTMCell(dim, dim, bias = True)
+        self.proj_gate = BRAGate(dim)
+        self.ff_gate = BRAGate(dim)
 
         self.input_proj = nn.Linear(dim + dim_state, dim, bias = False)
         self.state_proj = nn.Linear(dim + dim_state, dim, bias = False)
@@ -95,7 +114,6 @@ class BlockRecurrentAttention(nn.Module):
         state: Optional[TensorType['state_size', -1, 'state_dim']] = None,
         mask = None,
         state_mask = None,
-        attn_mask = None,
         rel_pos = None,
         rotary_pos_emb = None,
         prev_attn = None,
@@ -113,129 +131,9 @@ class BlockRecurrentAttention(nn.Module):
         projected_state = self.state_proj(torch.concat((state_as_q_cross_attn, state_attn), dim=2))
 
         input_residual = projected_input + x
-        state_residual = self.proj_gate(state + projected_state)
+        state_residual = self.proj_gate(projected_state, state)
 
         output = self.input_ff(input_residual) + input_residual
-        next_state = self.ff_gate()
+        next_state = self.ff_gate(self.state_ff(state_residual), state_residual)
 
-
-
-        return output
-
-
-
-
-
-
-
-
-
-    """
-        b, n, _, h, head_scale, scale, device, has_state = (
-            *x.shape,
-            self.heads,
-            self.head_scale,
-            self.scale,
-            x.device,
-            exists(state)
-        )
-        kv_input = default(state, x)
-
-        q_input = x
-        k_input = kv_input
-        v_input = kv_input
-
-        if exists(mem):
-            k_input = torch.cat((mem, k_input), dim = -2)
-            v_input = torch.cat((mem, v_input), dim = -2)
-
-        q = self.to_q(q_input)
-        k = self.to_k(k_input)
-        v = self.to_v(v_input)
-
-        q = rearrange(q, 'b n (h d) -> b h n d', h = h)
-
-        if exists(rotary_pos_emb) and not has_state:
-            l = rotary_pos_emb.shape[-1]
-            (ql, qr), (kl, kr), (vl, vr) = map(lambda t: (t[..., :l], t[..., l:]), (q, k, v))
-            ql, kl, vl = map(lambda t: apply_rotary_pos_emb(t, rotary_pos_emb), (ql, kl, vl))
-            q, k, v = map(lambda t: torch.cat(t, dim = -1), ((ql, qr), (kl, kr), (vl, vr)))
-
-        input_mask = None
-        if any(map(exists, (mask, state_mask))):
-            q_mask = default(mask, lambda: torch.ones((b, n), device = device).bool())
-            k_mask = q_mask if not exists(state) else state_mask
-            k_mask = default(k_mask, lambda: torch.ones((b, k.shape[-2]), device = device).bool())
-            q_mask = rearrange(q_mask, 'b i -> b 1 i 1')
-            k_mask = rearrange(k_mask, 'b j -> b 1 1 j')
-            input_mask = q_mask * k_mask
-
-        if self.qk_norm:
-            q, k = map(l2norm, (q, k))
-            scale = 1 / (self.scale.exp().clamp(min = 1e-2))
-
-        kv_einsum_eq = 'b h j d' if not self.one_kv_head else 'b j d'
-
-        dots = einsum(f'b h i d, {kv_einsum_eq} -> b h i j', q, k) * scale
-        mask_value = max_neg_value(dots)
-
-        if exists(prev_attn):
-            dots = dots + prev_attn
-
-        pre_softmax_attn = dots.clone()
-
-        if exists(rel_pos):
-            dots = rel_pos(dots)
-
-        if exists(input_mask):
-            dots.masked_fill_(~input_mask, mask_value)
-            del input_mask
-
-        if exists(attn_mask):
-            assert 2 <= attn_mask.ndim <= 4, 'attention mask must have greater than 2 dimensions but less than or equal to 4'
-            if attn_mask.ndim == 2:
-                attn_mask = rearrange(attn_mask, 'i j -> 1 1 i j')
-            elif attn_mask.ndim == 3:
-                attn_mask = rearrange(attn_mask, 'h i j -> 1 h i j')
-            dots.masked_fill_(~attn_mask, mask_value)
-
-        if exists(self.max_attend_past):
-            i, j = dots.shape[-2:]
-            range_q = torch.arange(j - i, j, device = device)
-            range_k = torch.arange(j, device = device)
-            dist = rearrange(range_q, 'i -> 1 1 i 1') - rearrange(range_k, 'j -> 1 1 1 j')
-            mask = dist > self.max_attend_past
-            dots.masked_fill_(mask, mask_value)
-            del mask
-
-
-        i, j = dots.shape[-2:]
-        r = torch.arange(i, device = device)
-        mask = rearrange(r, 'i -> 1 1 i 1') < rearrange(r, 'j -> 1 1 1 j')
-        mask = F.pad(mask, (j - i, 0), value = False)
-        dots.masked_fill_(mask, mask_value)
-        del mask
-
-        attn = self.attn_fn(dots, dim = -1)
-        post_softmax_attn = attn.clone()
-
-        attn = self.dropout(attn)
-
-        out = einsum(f'b h i j, {kv_einsum_eq} -> b h i d', attn, v)
-
-        if head_scale:
-            out = out * self.head_scale_params
-
-        out = rearrange(out, 'b h n d -> b n (h d)')
-
-        if exists(self.to_v_gate):
-            gates = self.to_v_gate(x)
-            out = out * gates.sigmoid()
-
-        intermediates = Intermediates(
-            pre_softmax_attn = pre_softmax_attn,
-            post_softmax_attn = post_softmax_attn
-        )
-
-        return self.to_out(out), intermediates
-    """
+        return output, next_state
